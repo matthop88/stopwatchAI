d0537e1bf61bfdb81d95639cd42ce4acabd2eaf0
Matthew Hopkins <matthop88@gmail.com>
2025-11-26 16:41:00 -0800

    Refactoring #30: Lined up all equals signs in code

--- jupiter/main.lua
+++ jupiter/main.lua
@@ -5,12 +5,12 @@
 --                     Local Variables                    --
 ------------------------------------------------------------
 
-local WINDOW_WIDTH = 300
+local WINDOW_WIDTH  = 300
 local WINDOW_HEIGHT = 400
-local COLOR_SALMON = {0.8, 0.5, 0.5}
-local COLOR_WHITE = {1, 1, 1}
+local COLOR_SALMON  = {0.8, 0.5, 0.5}
+local COLOR_WHITE   = {1, 1, 1}
 
-local elapsedTime = 0  -- Time in seconds
+local elapsedTime   = 0  -- Time in seconds
 local TIME_FONT
 
 ------------------------------------------------------------
@@ -54,13 +54,13 @@ function drawBackground()
 end
 
 function drawTimeText()
-    local timeText = getTimeTextMMSS()
+    local timeText   = getTimeTextMMSS()
     
-    local textWidth = TIME_FONT:getWidth(timeText)
+    local textWidth  = TIME_FONT:getWidth(timeText)
     local textHeight = TIME_FONT:getHeight()
     
-    local centeredX = (WINDOW_WIDTH - textWidth) / 2
-    local centeredY = (WINDOW_HEIGHT - textHeight) / 2
+    local centeredX  = (WINDOW_WIDTH - textWidth) / 2
+    local centeredY  = (WINDOW_HEIGHT - textHeight) / 2
     
     love.graphics.setColor(COLOR_WHITE)
     love.graphics.print(timeText, centeredX, centeredY)
@@ -68,8 +68,8 @@ end
 
 function getTimeTextMMSS()
     local totalSeconds = math.floor(elapsedTime)
-    local minutes = math.floor(totalSeconds / 60)
-    local seconds = totalSeconds % 60
+    local minutes      = math.floor(totalSeconds / 60)
+    local seconds      = totalSeconds % 60
     
     return string.format("%02d:%02d", minutes, seconds)
 end
